fbbf385f0e9d190d413827b32599db96091bbd3c
Najma Abdi <anajmabdi@gmail.com>
2023-03-05 15:56:39 -0800

    views, controllers, and logic for a list of restaurants for different cuisines
    Co-Authored-By: Cameron Abel <cameronabel@gmail.com>

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId {get; set;}
+    public string Name {get; set;}
+    public Cuisine Cuisine {get; set;}
+    public int CuisineId {get; set;}
+    
+  }
+}
